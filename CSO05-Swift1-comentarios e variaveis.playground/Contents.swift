773be7a92a841c1da9a5498b38a4a33ea9302330
CauaSDO <cauasilva@icloud.com>
2022-01-04 17:40:22 -0300

    Initial Commit

---
+++ CSO05-Swift1-comentarios e variaveis.playground/Contents.swift
@@ -0,0 +1,28 @@
+
+import UIKit
+
+
+var greeting = "Hello, playground"
+
+
+//COMENTARIOS
+/*
+ O comando abaixo serve para imprimir no console.
+ use print a (variavel que será impressa
+//print(greeting)
+ //Swift: Lançada em 2014, atualmente na versão 4.0
+ */
+
+//VARIAVEIS
+var nome = "Cãua Silva"
+print(nome)
+var age = 14
+var 🐶 = "billy"
+print("O nome do meu cachorro é, 🐶")
+var 👶 = 1
+var 👼 = 2
+var sum = 👶 + 👼
+var myFatherAge = 58
+
+
+
